8dc8f66e56d846d1e3de5a587797b5ed7d1167a5
dave-lanigan <dav.lanigan@gmail.com>
2024-02-05 10:51:24 +0000

    debug lambda extensions issues

--- lambchop/server.py
+++ lambchop/server.py
@@ -1,67 +1,87 @@
-import functools
 import importlib
-import inspect
 import sys
 from pathlib import Path
 
-import anyio
+from http.server import HTTPServer, BaseHTTPRequestHandler
+from queue import Queue
+import threading
+
 import dill
-from anyio.abc import SocketAttribute
+import json
 
 from lambchop.datastructures import Task
 
 
-class Server:
+def deserialize(msg: bytes) -> Task:
+    msg = json.loads(msg)
+    msg = bytes.fromhex(msg)
+    msg = dill.loads( msg )
+    return msg
+
+def get_fun(task: Task) -> None:
+    f = Path(task.file)
+    sys.path.append(str(f.parent))
+    module = importlib.import_module(f.stem)
+    return getattr(module, task.func)
+
+def execute(task):
+    print("Task executing ...", flush=True)
+    func = get_fun(task)
+    args = task.args
+    kwargs = task.kwargs
+    func(*args, **kwargs)
+
+
+class Handler(BaseHTTPRequestHandler):
+    """Handler recieves jobs and adds them to a queue."""
+    def __init__(self, request, client_address, server, q: Queue):
+        self.q=q
+        super().__init__(request, client_address, server)
+
+    def _set_response(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'application/json')
+        self.end_headers()
+    
+    def do_GET(self):
+        self._set_response()
+        msg = json.dumps('PONG').encode('utf-8')
+        self.wfile.write(msg)
+
+    def do_POST(self):
+        """Sends a task to the queue."""
+        print("msg recieved. Adding to queue...", flush=True)
+        content_length = int(self.headers['Content-Length'])
+        msg = self.rfile.read(content_length)
+        task = deserialize(msg)
+        
+        t = threading.Thread(target=execute, args=(task,))
+        t.start()
+        self.q.append(t)
+
+        self._set_response()
+        msg = json.dumps('TASK QUEUED.').encode('utf-8')
+        self.wfile.write(msg)
+
+
+class Server(HTTPServer):
     def __init__(self, port: int = 1956) -> None:
-        self.port = port
+        addr = ("0.0.0.0", port)
+        self.q = []
+        
+        def handler(*args):
+            Handler(*args, q=self.q)
 
-    def deserialize(self, task: Task) -> None:
-        return dill.loads(task)
+        self.server = HTTPServer(addr, handler)
 
-    def get_fun(self, task: Task) -> None:
-        f = Path(task.file)
-        sys.path.append(str(f.parent))
-        module = importlib.import_module(f.stem)
-        return getattr(module, task.func)
-
-    async def execute(self, msg) -> None:
-        task: Task = self.deserialize(msg)
-
-        func = self.get_fun(task)
-        args = task.args
-        kwargs = task.kwargs
-
-        if inspect.iscoroutinefunction(func):
-            await func(*args, **kwargs)
-        else:
-            if kwargs:  # pragma: no cover
-                # run_sync doesn't accept 'kwargs', so bind them in here
-                func = functools.partial(func, **kwargs)
-            return await anyio.to_thread.run_sync(func, *args)
-
-    async def handle(self, client):
-        async with client:
-            print(
-                "receiving message from", client.extra(SocketAttribute.remote_address)
-            )
-            msg = await client.receive()
-            if msg == b"PING":
-                await client.send(b"PONG")
-                return
-            try:
-                await self.execute(msg)
-                await client.send(b"COMPLETED")
-            except Exception as e:
-                print(e)
-                await client.send(b"ERROR")
-
-    async def serve(self):
-        print("Starting server.")
-        listener = await anyio.create_tcp_listener(local_port=self.port)
-        print(f"Listening on port: {self.port}")
-        await listener.serve(self.handle)
+    def serve(self):
+        print("Starting server...", flush=True)
+        self.server.serve_forever()
+        print("Server stopped.", flush=True)
 
 
-if __name__ == "__main__":
+if __name__=="__main__":
+    from datastructures import Task
+    
     s = Server()
-    anyio.run(s.serve)
+    s.serve()
